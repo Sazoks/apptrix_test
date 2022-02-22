20abdd88a20ec92a5ded072bff5b611ad185e93a
Alexey Bobrov <bobrov.bobrov-alex@yandex.ru>
2022-02-23 00:03:47 +0700

    Настроен доступ к media-файлам.

--- date_me_please/date_me_please/urls.py
+++ date_me_please/date_me_please/urls.py
@@ -18,8 +18,14 @@ from django.urls import (
     path,
     include,
 )
+from django.conf.urls.static import (
+    static,
+    settings,
+)
 
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
