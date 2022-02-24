a44440a4976cd2e684133c5e7ca8ffefad0e698b
Alexey Bobrov <bobrov.bobrov-alex@yandex.ru>
2022-02-24 12:36:56 +0700

    Изменена структура URL в соответствии с ТЗ.

--- date_me_please/api/clients/urls.py
+++ date_me_please/api/clients/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.UserListView.as_view(), name='api_user_list'),
     path('me/lovers/', views.LoverListView.as_view(), name='api_lovers'),
     path('<int:pk>/match/', views.LikeUserView.as_view(), name='api_like_user'),
     path('<int:pk>/', views.UserDetailView.as_view(), name='api_user_detail'),
--- date_me_please/api/urls.py
+++ date_me_please/api/urls.py
@@ -7,7 +7,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from .clients import views
+
+
 urlpatterns = [
+    path('list/', views.UserListView.as_view(), name='api_user_list'),
     path('clients/', include('api.clients.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
